b0f13f6705fedebe1bb069d01927c261f930aa98
devanych <mail@devanych.ru>
2020-11-16 23:00:09 +0300

    Fix line breaks for XML in DummyDataHelper

--- tests/TestAsset/DummyDataHelper.php
+++ tests/TestAsset/DummyDataHelper.php
@@ -12,6 +12,6 @@ final class DummyDataHelper
     public static function xml(string $data, string $version = '1.0', string $encoding = 'UTF-8'): string
     {
         $startLine = sprintf('<?xml version="%s" encoding="%s"?>', $version, $encoding);
-        return $startLine . PHP_EOL . preg_replace('/(?!item)\s(?!key)/', '', $data) . PHP_EOL;
+        return $startLine . "\n" . preg_replace('/(?!item)\s(?!key)/', '', $data) . "\n";
     }
 }
