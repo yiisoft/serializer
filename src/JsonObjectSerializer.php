a2893760d878ab1af75f427020149050af9658d5
devanych <mail@devanych.ru>
2020-11-17 15:10:43 +0300

    Add JsonObjectSerializer

---
+++ src/JsonObjectSerializer.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Serializer;
+
+use Symfony\Component\Serializer\Encoder\JsonDecode;
+use Symfony\Component\Serializer\Encoder\JsonEncode;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+use const JSON_UNESCAPED_SLASHES;
+use const JSON_UNESCAPED_UNICODE;
+
+/**
+ * JsonObjectSerializer serializes objects in JSON format and vice versa.
+ */
+final class JsonObjectSerializer implements ObjectSerializerInterface
+{
+    use ObjectSerializerTrait;
+
+    /**
+     * @param int $options The encoding options.
+     * @see http://www.php.net/manual/en/function.json-encode.php
+     */
+    public function __construct(int $options = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)
+    {
+        $this->serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder(
+            new JsonEncode([JsonEncode::OPTIONS => $options]),
+            new JsonDecode([JsonDecode::ASSOCIATIVE => true]),
+        )]);
+    }
+
+    public function serialize(object $object): string
+    {
+        return $this->serializer->serialize($object, JsonEncoder::FORMAT);
+    }
+
+    public function serializeMultiple(array $objects): string
+    {
+        $this->checkArrayObjects($objects);
+        return $this->serializer->serialize($objects, JsonEncoder::FORMAT);
+    }
+
+    public function unserialize(string $value, string $class): object
+    {
+        $value = $this->checkAndDecodeData($value, $class, JsonEncoder::FORMAT);
+        return $this->denormalizeObject($value, $class);
+    }
+
+    public function unserializeMultiple(string $value, string $class): array
+    {
+        $value = $this->checkAndDecodeData($value, $class, JsonEncoder::FORMAT);
+        return $this->denormalizeObjects($value, $class);
+    }
+}
---
+++ tests/JsonObjectSerializerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Serializer\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Serializer\Tests\TestAsset\DummyData;
+use Yiisoft\Serializer\JsonObjectSerializer;
+
+final class JsonObjectSerializerTest extends TestCase
+{
+    private JsonObjectSerializer $serializer;
+
+    public function setUp(): void
+    {
+        $this->serializer = new JsonObjectSerializer();
+    }
+
+    public function testSerializeAndUnserialize(): void
+    {
+        $object = new DummyData('foo', 99, 1.1, [1, 'bar' => 'baz'], false);
+        $json = json_encode([
+            'string' => $object->getString(),
+            'int' => $object->getInt(),
+            'float' => $object->getFloat(),
+            'array' => $object->getArray(),
+            'bool' => $object->isBool(),
+        ]);
+
+        $this->assertSame($json, $this->serializer->serialize($object));
+        $this->assertEquals($object, $this->serializer->unserialize($json, DummyData::class));
+    }
+
+    public function testSerializeMultipleAndUnserializeMultiple(): void
+    {
+        $objects = [
+            $object1 = new DummyData('foo', 99, 1.1, [1], false),
+            $object2 = new DummyData('bar', 10, 2.2, [1, 2, 3], true),
+            $object3 = new DummyData('baz', 0, 3.3, ['foo' => 'bar'], false),
+        ];
+        $json = json_encode([
+            [
+                'string' => $object1->getString(),
+                'int' => $object1->getInt(),
+                'float' => $object1->getFloat(),
+                'array' => $object1->getArray(),
+                'bool' => $object1->isBool(),
+            ],
+            [
+                'string' => $object2->getString(),
+                'int' => $object2->getInt(),
+                'float' => $object2->getFloat(),
+                'array' => $object2->getArray(),
+                'bool' => $object2->isBool(),
+            ],
+            [
+                'string' => $object3->getString(),
+                'int' => $object3->getInt(),
+                'float' => $object3->getFloat(),
+                'array' => $object3->getArray(),
+                'bool' => $object3->isBool(),
+            ],
+        ]);
+
+        $this->assertSame($json, $this->serializer->serializeMultiple($objects));
+        $this->assertEquals($objects, $this->serializer->unserializeMultiple($json, DummyData::class));
+    }
+}
