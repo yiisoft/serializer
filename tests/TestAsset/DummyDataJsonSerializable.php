22592ac0abe8f787e7b0549c801a2c0f7ca7475f
devanych <mail@devanych.ru>
2020-11-18 19:35:51 +0300

    Add functionality to JsonSerializer

---
+++ tests/TestAsset/DummyDataJsonSerializable.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Serializer\Tests\TestAsset;
+
+use JsonSerializable;
+
+final class DummyDataJsonSerializable implements JsonSerializable
+{
+    private string $data;
+
+    public function __construct(string $data)
+    {
+        $this->data = $data;
+    }
+
+    public function getData(): string
+    {
+        return $this->data;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'data' => 'jsonSerialize'
+        ];
+    }
+}
